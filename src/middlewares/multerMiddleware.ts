442287e60dc115a4dc6f8150d2eb5198000b655a
prakosodwika <prakosodwika4@gmail.com>
2025-04-30 14:30:52 +0800

    feat: multer in middleware

--- src/middlewares/multerMiddleware.ts
+++ src/middlewares/multerMiddleware.ts
@@ -2,7 +2,11 @@ import { Request, Response, NextFunction } from "express";
 import { response_bad_request } from "$utils/response.utils";
 import { upload } from "$utils/file.utils";
 
-export function validateUpload(req: Request, res: Response, next: NextFunction) {
+export function uploadFile(
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+): Response | void {
   upload.single("file")(req, res, (err) => {
     if (err instanceof Error) {
       return response_bad_request(res, err.message);
